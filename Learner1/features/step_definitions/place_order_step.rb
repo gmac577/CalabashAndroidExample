d27af944592be2d9fb1a68ae87d8161dbd3a4990
linwatt <lwatters@riis.com>
2015-07-24 13:52:43 -0400

    Adding New step definition file

---
+++ Learner1/features/step_definitions/place_order_step.rb
@@ -0,0 +1,35 @@
+Given(/^I am on the order type screen$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the sandwich radio button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The next button becomes enabled$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the salad radio button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on order screen with sandwich slected$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click the Next button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The Bread page opens$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on order screen with salad slected$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The Salad Type page opens$/) do
+  pending # express the regexp above with the code you wish you had
+end
