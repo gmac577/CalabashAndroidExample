2e383504dad2fba7b28d7cdaf9ead368662b922a
Gordon McCowey <gmccowey@riis.com>
2015-07-30 00:54:44 -0400

    Re-setting Calabash Demo Project files

--- Learner1/features/support/page_populator.rb
+++
@@ -1,9 +0,0 @@
-module PagePopulator
-    include DataMagic
-
-    def select_bread_with(data)
-      data.each do |key, value|
-        element = "CheckedTextView text:'#{key}'"
-      end
-    end
-end
